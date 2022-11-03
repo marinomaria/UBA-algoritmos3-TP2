520220a9852e2e417b105ddb9db373a08e0a4f56
María Marino <mariamarino@Marias-MacBook-Air.local>
2022-11-03 00:00:20 -0300

    retoco ej2.cpp y agrego ej2Structured.cpp para migrar a la versión estructurada

--- ej2.cpp
+++ ej2.cpp
@@ -14,51 +14,51 @@ using Arista = pair<size_t,size_t>;
 vector<vector<int>> M;
 
 //Estructura Union-Find.
-struct UF {
+struct unionFind {
     //vectores internos
-    //  p: Vector con los parents
-    //  s: Vector con tamaños
-    //  m: Vector con arista mínima
-    vector<int> p, s, m;
-    //  h: Vector con todos los hijos
-	vector<list<int>> h; //tiene que ser un vector [[0],[1],[2],[3]...]
+    //  parent: Vector con los parents
+    //  setSize: Vector con tamaños
+    //  minEdge: Vector con arista mínima
+    vector<int> parent, setSize, minEdge;
+    //  descendants: Vector con todos los hijos
+	vector<list<int>> descendants; //tiene que ser un vector [[0],[1],[2],[3]...]
     
     //Constructor
-    UF(int n): p(n, -1), s(n, 1), m(n, INF) {} //falta el constructor de h
+    explicit unionFind(int n): parent(n, -1), setSize(n, 1), minEdge(n, INF), descendants(n) {} //falta el constructor de descendants
     
     //unite: Hace la unión, teniendo en cuenta todas las variables internas
-    int unite(int v, int w) {
+    void unite(int v, int w) {
         v = find(v); w = find(w);
-        if(v == w) return s[v];
-        if(s[v] < s[w]) swap(v, w);
-        p[w] = v; //El árbol de mayor tamaño queda como padre
+        if(v == w) return;
+        if(setSize[v] < setSize[w]) swap(v, w);
+        parent[w] = v; //El árbol de mayor tamaño queda como padre
 		//Acá hace un doble for para poder mantener cuál es el arista mínima del set
 		int arista_minima = INF;
-		for (auto i : h[v]) {
-			for (auto j : h[w]){
+		for (auto i : descendants[v]) {
+			for (auto j : descendants[w]){
 				if (M[i][j] < arista_minima) arista_minima = M[i][j];
 			}
 		}
-		m[v] = min({arista_minima, m[v], m[w]});
+        minEdge[v] = min({arista_minima, minEdge[v], minEdge[w]});
 
-        s[v] += s[w];
-		//h me enumera todos los nodos que son parte del set
-        h[v].splice(h[v].end(), h[w]);
+        setSize[v] += setSize[w];
+		//descendants me enumera todos los nodos que son parte del set
+        descendants[v].splice(descendants[v].end(), descendants[w]);
     }
     
     //find: Devuelve la raiz del set
     int find(int v) {
-        return p[v] == -1 ? v : p[v] = find(p[v]); //Recursión para path compression
+        return parent[v] == -1 ? v : parent[v] = find(parent[v]); //Recursión para path compression
     }
 
     int minima_arista(int v) {
 		v = find(v);
-        return m[v];
+        return minEdge[v];
     }
 
 	int size(int v) {
 		v = find(v);
-		return s[v];
+		return setSize[v];
 	}
 
 };
@@ -69,7 +69,7 @@ int main() {
     size_t n,m;
     cin >> n >> m;
 
-    UF componentes(n+1);                //Para indexar desde 1
+    unionFind componentes(n + 1);                //Para indexar desde 1
     vector<pair<Costo, Arista>> valores(m);
 
     //Lectura del vector de aristas
---
+++ ej2Structured.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <list>
+
+using namespace std;
+
+#define INF numeric_limits<int>::max()
+
+struct edge {
+    int u;
+    int v;
+    // int weight;
+    // Avoid redundancy by keeping an adjacency matrix with weights OR a list of edges with their weights, not both!
+};
+
+using Matrix = vector<vector<int>>;
+Matrix weight;
+
+struct unionFind {
+    vector<int> parent, setSize, minEdge;
+    vector<list<int>> descendants;
+
+    explicit unionFind(int n): parent(n, -1), setSize(n, 1), minEdge(n, INF), descendants(n) {}
+
+    void unite(int v, int w) {
+        v = find(v); w = find(w);
+        //  if(v == w) return;
+        // No need to make it robust as unite() will be called if and only if v and w are from different sets
+        if (setSize[v] < setSize[w]) swap(v, w);
+        parent[w] = v;
+        setSize[v] += setSize[w];
+        descendants[v].splice(descendants[v].end(), descendants[w]);
+
+        int newMinEdge = INF;
+        for (auto i : descendants[v]) {
+            for (auto j : descendants[w]){
+                if (weight[i][j] < newMinEdge) newMinEdge = weight[i][j];
+            }
+        }
+        minEdge[v] = min({newMinEdge, minEdge[v], minEdge[w]});
+
+    }
+
+    int find(int v) {
+        return parent[v] == -1 ? v : parent[v] = find(parent[v]);
+    }
+
+    int minima_arista(int v) {
+        v = find(v);
+        return minEdge[v];
+    }
+
+    int size(int v) {
+        v = find(v);
+        return setSize[v];
+    }
+
+};
+
+int T, N, M;
+
+int solveTourBelt(int &n, vector<edge> m) {
+    int sumTourBeltSize = 0;
+
+    return sumTourBeltSize;
+}
+
+int main() {
+    cin >> T;
+    for(int i = 0; i < T; i++) {
+        cin >> N >> M;
+        vector<edge> edges;
+        weight = Matrix(N, vector<int>(N, -1));
+        for (int j = 0; j < M; j++) {
+            int v, w, k;
+            cin >> v >> w >> k;
+            edges.push_back({v, w});
+            weight[v][w] = k;
+        }
+
+
+        cout << solveTourBelt(N, edges) << endl;
+
+    }
+    return 0;
+}
