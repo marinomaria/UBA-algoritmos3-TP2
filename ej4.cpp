b85b1dec0e9457fe79f24190d2c908dc59485c17
María Marino <mariamarino@Marias-MacBook-Air.local>
2022-11-03 16:31:30 -0300

    arreglo indices

--- ej4.cpp
+++ ej4.cpp
@@ -6,7 +6,7 @@ using namespace std;
 #define INF numeric_limits<int>::max()
 
 struct constraint {
-    // Represents constraint X[i] - X[j] ≤ b
+    // Represents constraint X[j] - X[i] ≤ b
     int i;
     int j;
     int b;
@@ -24,10 +24,16 @@ ostream &operator<<(ostream &out, const vector<int> &vec) {
     return out;
 }
 
+vector<int> tail(const vector<int> &v) {
+    return {v.begin() + 1, v.end()};
+}
+
 int findK(const constraint &c, const vector<int> &D, const vector<int> &X) {
     int k = INF;
-    for (int i = D.size(); i >= 0; i--) {
-        if (D[i] <= X[c.i] + c.b) k = i;
+    for (int i = D.size() - 1; i >= 1; i--) {
+        if (D[i] <= X[c.i] + c.b) {
+            k = i; break;
+        }
     }
 
     return k;
@@ -37,25 +43,27 @@ fishburnOutput fishburnSolver(int &variableCount, vector<constraint> &inequaliti
     vector<int> X(variableCount + 1, D[D.size() - 1]);
     bool satisfiable = true;
 
-    bool changed = true;
-    while (changed) {
+    bool changed;
+    do {
         changed = false;
-        for (constraint c : inequalities) {
+        for (int l = 1; l <= inequalities.size(); l++) {
+            constraint c = inequalities[l];
             int k = findK(c, D, X);
-            if (X[c.i] > X[c.j] + c.b && k < D.size()) {
+            if (X[c.j] > X[c.i] + c.b && k < D.size()) {
                 X[c.j] = D[k];
                 changed = true;
             }
         }
-    }
+    } while (changed);
 
-    for (constraint c : inequalities) {
-        if (X[c.i] - X[c.j] > c.b) {
+    for (int l = 1; l <= inequalities.size(); l++) {
+        constraint c = inequalities[l];
+        if (X[c.j] - X[c.i] > c.b) {
             satisfiable = false;
         }
     }
 
-    return {satisfiable, X};
+    return {satisfiable, tail(X)};
 }
 
 int main() {
@@ -65,20 +73,21 @@ int main() {
         cin >> k >> n >> m;
 
         vector<constraint> constraints;
+        constraints.push_back({-1,-1,-1});
         for (int j = 0; j < k; j++) {
             int a, b, c;
             cin >> a >> b >> c;
-            constraints.push_back({a, b, c});
+            constraints.push_back({b, a, c});
         }
 
         vector<int> D;
+        D.push_back(-1);
         for (int l = 0; l < m; l++) {
             int d;
             cin >> d;
             D.push_back(d);
         }
 
-        sort(D.begin(), D.end());
         fishburnOutput s = fishburnSolver(n, constraints, D);
         if (s.satisfiable) {
             cout << s.solution << endl;
