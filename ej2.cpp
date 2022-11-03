7411983d8526edbb7896477a5780bc3ec16922d5
María Marino <mariamarino@ip-192-168-0-145.ec2.internal>
2022-11-03 13:44:00 -0300

    reemplazo ej2.cpp por ej2Structured.cpp

--- ej2.cpp
+++ ej2.cpp
@@ -1,123 +1,121 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <list>
+#include <set>
 
 using namespace std;
 
-using Costo = unsigned long int;
-using Arista = pair<size_t,size_t>;
+#define INF numeric_limits<int>::max()
 
-// M = matriz de adyacencia TODO
-vector<vector<int>> M;
+using Matrix = vector<vector<int>>;
+Matrix weight;
 
-//Estructura Union-Find.
-struct UF {
-    //vectores internos
-    //  p: Vector con los parents
-    //  s: Vector con tamaños
-    //  m: Vector con arista mínima
-    vector<int> p, s, m;
-    //  h: Vector con todos los hijos
-	vector<vector<int>> h; //tiene que ser un vector [[0],[1],[2],[3]...]
-    
-    //Constructor
-    UF(int n): p(n, -1), s(n, 1), m(n, INF) {} //falta el constructor de h
-    
-    //unite: Hace la unión, teniendo en cuenta todas las variables internas
-    int unite(int v, int w) {
-        v = find(v); w = find(w);
-        if(v == w) return s[v];
-        if(s[v] < s[w]) swap(v, w);
-        p[w] = v; //El árbol de mayor tamaño queda como padre
-		//Acá hace un doble for para poder mantener cuál es el arista mínima del set
-		int arista_minima = INF;
-		for (auto i : h[v]) {
-			for (auto j : h[w]){
-				if (M[i][j] < arista_minima){arista_minima = M[i][j]}
-			}
-		}
-		m[v] = min(arista_minima,m[v],m[w]);
+struct edge {
+    int u;
+    int v;
 
-        s[v] += s[w];
-		//h me enumera todos los nodos que son parte del set
-		h[v] = h[v] append h[w]; //Quiero appendear
-		//Si h son listas enlazadas, anda seguro (arrays tal vez también)
+    bool operator > (const edge &other) const {
+        return (weight[u][v] > weight[other.u][other.v]);
     }
-    
-    //find: Devuelve la raiz del set
-    int find(int v) {
-        return p[v] == -1 ? v : p[v] = find(p[v]); //Recursión para path compression
-    }
-
-    int minima_arista(int v) {
-		v = componentes.find(v);
-        return m[v];
-    }
-
-	int nodos_del_set(int v) {
-		v = componentes.find(v);
-		return h[v];
-	}
-
-	int size(int v) {
-		v = componentes.find(v);
-		return s[v];
-	}
-
 };
 
 
-int main() {
-	//Esto no lo toqué
-    size_t n,m;
-    cin >> n >> m;
+struct unionFind {
+    vector<int> parent, size, minEdge;
+    vector<list<int>> descendants;
 
-    UF componentes(n+1);                //Para indexar desde 1
-    vector<pair<Costo, Arista>> valores(m);
-
-    //Lectura del vector de aristas
-	//for (auto& [c, e] : valores) cin >> e.first >> e.second >> c;
-
-    //Ordenamiento de las aristas por peso
-    sort(valores.begin(), valores.end()); // dar vuelta
-	//Esto no lo toqué
-	
-	int suma_total = 0;
-
-
-    //Recorre las aristas ordenadas y se queda con aquellas que unan
-    //dos vértices de componentes conexas distintas, uniéndolas
-	vector<int> this_level_trees; //Acá guardo todos los árboles que fueron expandidos para este costo de arista
-	for (int i=0; i<(int)size(valores); i++){
-		auto [c,e] = valores[i];
-        auto [i,j] = e;
-        if (componentes.find(i) != componentes.find(j)) {
-            componentes.unite(i,j);
-        }
-		this_level_trees.push_back(componentes.find(j));
-
-		//Si es la última arista de este peso, paso a sumar todo
-		int peso_proxima_arista = valores[i+1][0];//falta el último caso
-		if (c > peso_proxima_arista){
-			//Acá necesito convertir this_level_trees en una lista de raíces sin repetir
-			for (int i = 0; i<size(this_level_trees); i++){
-				this_level_trees[i] = componentes.find(this_level_trees[i])
-			}
-			this_level_trees = std::unique(this_level_trees) //HACER QUE ESTO ANDE
-
-
-			for (auto t : this_level_trees){
-				if (componentes.arista_minima(t) == c) {suma_total = suma_total + componentes.size(t) }
-			}
-		}
+    explicit unionFind(int n): parent(n, -1), size(n, 1), minEdge(n, INF) {
+        for (int i = 0; i < n; i++) descendants.push_back({i});
     }
 
-    //Salida en formato CSES
-    //if (mst.size()<n-1) {
-        //cout << "IMPOSSIBLE";
-    //} else {
-        //cout << costo_total;
-    //}
+    ;
+    void unite(int v, int w) {
+        v = find(v); w = find(w);
+        if (size[v] < size[w]) swap(v, w); // Make sure w is always 'smaller' than v
+        parent[w] = v;
+        size[v] += size[w];
 
-    //return 0;
+        int newMinEdge = INF;
+        for (auto i : descendants[v]) {
+            for (auto j : descendants[w]){
+                if (weight[i][j] < newMinEdge) newMinEdge = weight[i][j];
+            }
+        }
+        minEdge[v] = min({newMinEdge, minEdge[v], minEdge[w]});
+
+        descendants[v].splice(descendants[v].end(), descendants[w]);
+    }
+
+    int find(int v) {
+        return parent[v] == -1 ? v : parent[v] = find(parent[v]);
+    }
+
+    int getMinEdge(int v) {
+        v = find(v);
+        return minEdge[v];
+    }
+
+    int getSize(int v) {
+        v = find(v);
+        return size[v];
+    }
+
+};
+
+int T, N, M;
+
+int solveTourBelt(int &n, vector<edge> &m) {
+    int sumTourBeltSize = 0;
+
+    sort(m.begin(), m.end(), greater<edge>());
+
+    unionFind sets(n + 1);
+
+    vector<int> modifiedTrees;
+    for (int i = 0; i < m.size(); i++) {
+        edge e = m[i];
+        if (sets.find(e.u) != sets.find(e.v)) {
+            sets.unite(e.u, e.v);
+        }
+        modifiedTrees.push_back(sets.find(e.u));
+
+        if (i == m.size() - 1 || weight[m[i + 1].u][m[i + 1].v]) {
+            set<int> uniqueModifiedTrees;
+            for (int t : modifiedTrees) {
+                uniqueModifiedTrees.emplace(sets.find(t));
+            }
+
+            set<int>::iterator it;
+            for (it = uniqueModifiedTrees.begin(); it != uniqueModifiedTrees.end(); it++) {
+                if (sets.getMinEdge(*it) == weight[e.u][e.v]) {
+                    sumTourBeltSize += sets.getSize(*it);
+                }
+            }
+            modifiedTrees.clear();
+        }
+    }
+
+    return sumTourBeltSize;
+}
+
+int main() {
+    cin >> T;
+    for(int i = 0; i < T; i++) {
+        cin >> N >> M;
+        vector<edge> edges;
+        weight = Matrix(N + 1, vector<int>(N + 1, INF));
+        for (int j = 0; j < M; j++) {
+            int v, w, k;
+            cin >> v >> w >> k;
+            edges.push_back({v, w});
+            weight[v][w] = k;
+            weight[w][v] = k;
+        }
+
+
+        cout << solveTourBelt(N, edges) << endl;
+
+    }
+    return 0;
 }
